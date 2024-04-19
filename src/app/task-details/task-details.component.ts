ee8794a8aa7888124a93d6b56de14f5ff76907cf
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2024-04-19 15:50:56 -0300

    Criando componentes Details e filter

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from '../model/task';
+import { TaskService } from '../services/task.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent {
+  
+  constructor(private route: ActivatedRoute, private taskService: TaskService, private router: Router){}
 
+  task?: Task;
+
+  ngOnInit(){
+    let id = this.route.snapshot.paramMap.get('id')
+
+    if(id==null){
+      this.navigateBack()
+    }else{
+
+      this.task = this.taskService.getById(+id)
+      
+      if(this.task === undefined){
+        this.navigateBack()
+      }
+    }
+
+  }
+  private navigateBack(){
+    this.router.navigate(['/list'], {relativeTo: this.route})
+  }
+
+  save(){
+    this.taskService.updateTask()
+
+    this.navigateBack()
+  }
+
+  cancel(){
+    this.navigateBack()
+  }
 }
